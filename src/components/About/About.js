486f0ded21b76da72815db920b757fe29ce0bf03
Venu Koduru <venukoduru@Venus-Air.ht.home>
2025-04-14 13:11:34 -0500

    small change in Resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,27 +21,28 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+            <h1 style={{ fontSize: "2.4em", fontWeight: 600, paddingBottom: "20px" }}>
+              Know Who <strong className="purple">I AM</strong>
             </h1>
             <Aboutcard />
           </Col>
+
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
+            style={{ paddingTop: "80px", paddingBottom: "50px" }}
+            className="about-img text-center"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img src={laptopImg} alt="about" className="img-fluid rounded shadow" />
           </Col>
         </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
 
+        <h1 className="project-heading">
+          <strong className="purple">Professional</strong> Skillset
+        </h1>
         <Techstack />
 
         <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+          <strong className="purple">Tools</strong> I Use
         </h1>
         <Toolstack />
 
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/VENU_RESUME4.pdf";
+import pdf from "../../Assets/../Assets/Venu.Koduru_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
